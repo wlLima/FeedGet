2d7ab62ebf36afdd8be9acb30c25c42d1de05730
wlLima <lima14.wl@gmail.com>
2022-05-06 17:56:33 -0300

    Added icons package and widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget"
 
-function App() {
-  return <h1>Hello teste</h1>
+
+export function App() {
+  return(
+    <Widget />
+  )
 }
 
-export default App
---
+++ src/components/Widget.tsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from 'phosphor-react'
+
+export function Widget(){
+    return(
+      <button className="bg-violet-500 rounded-full px-3 h-12 text-white">
+          <ChatTeardropDots className="w-6 h-6" />
+      </button>
+    )
+}
